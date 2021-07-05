3da8f01b0fe024da37f81d77049b4dd0ac8fba39
Do Van Phu <abeobk@gmail.com>
2021-07-05 13:43:58 +0900

    update

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-find . -type f \( -iname "*.*" ! -path "./.git/*" ! -iname "checksum.dat" \) -exec bash -c "sha1sum '{}'" \; > checksum.dat
+find . -type f \( -iname "*" ! -path "./.git/*" ! -iname "checksum.dat" \) -exec bash -c "sha1sum '{}'" \; > checksum.dat
 git add -A
 git commit -m "update"
 git push
